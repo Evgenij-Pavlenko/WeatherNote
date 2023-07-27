c35699f24a960062fa34c6fca3db9b35b14ae4b5
ievgenii.pavlenko <evgenij.pavlenko@gmail.com>
2023-07-27 17:16:32 +0200

    add Database DAO

--- app/src/main/java/com/example/abschlussaufgabe/data/local/WeatherDatabaseDao.kt
+++ app/src/main/java/com/example/abschlussaufgabe/data/local/WeatherDatabaseDao.kt
@@ -1,7 +1,21 @@
 package com.example.abschlussaufgabe.data.local
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.abschlussaufgabe.data.datamodels.Weather
 
 @Dao
 interface WeatherDatabaseDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(weathers: List<Weather>)
+
+    @Query("SELECT * FROM weather_table")
+    fun getAll(): LiveData<List<Weather>>
+
+    @Query("DELETE FROM weather_table")
+    fun deleteAll()
 }
